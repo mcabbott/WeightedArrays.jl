f6d606dda4f34bceb5ad80591d4ca718f286c4d3
Michael Abbott <improbable@gmail.com>
2018-10-18 21:29:06 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,4 @@
-@info "build.jl is running!"
-
 using Pkg
 
-try
-    pkg"rm GroupSlices"
-    @info "removed old GroupSlices"
-catch
-    @info "didn't remov old GroupSlices"
-end
+pkg"add https://github.com/mcabbott/GroupSlices.jl"
 
-# Pkg.add(Pkg.PackageSpec(name="GroupSlices", url="https://github.com/mcabbott/GroupSlices.jl", rev="julia07fixes"))
-pkg"add https://github.com/mcabbott/GroupSlices.jl#julia07fixes"
-
-@info "added new GroupSlices"
